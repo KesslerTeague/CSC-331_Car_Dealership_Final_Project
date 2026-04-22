7f5f51f72aeca5a0343631590e88ca8df02c8bb1
Connor <connwarnick@gmail.com>
2026-04-22 09:26:14 -0400

    stub sale and inventory tests

--- src/test/java/edu/uncw/csc331/cardealership/model/InventoryTest.java
+++ src/test/java/edu/uncw/csc331/cardealership/model/InventoryTest.java
@@ -1,4 +1,31 @@
 package edu.uncw.csc331.cardealership.model;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class InventoryTest {
+
+    @Test
+    void testAddVehicle() {}
+
+    @Test
+    void testRemoveVehicle() {}
+
+    @Test
+    void testSearchByMake() {}
+
+    @Test
+    void testFilterByStatus() {}
+
+    @Test
+    void testUpdateVehicle() {}
+
+    @Test
+    void testViewAll() {}
+
+    @Test
+    void testRecordSale() {}
+
+    @Test
+    void testGetSales() {}
 }
--- src/test/java/edu/uncw/csc331/cardealership/model/SaleTest.java
+++ src/test/java/edu/uncw/csc331/cardealership/model/SaleTest.java
@@ -1,4 +1,28 @@
 package edu.uncw.csc331.cardealership.model;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SaleTest {
+
+    @Test
+    void testGetSaleId() {}
+
+    @Test
+    void testGetSaleDate() {}
+
+    @Test
+    void testGetVehicle() {}
+
+    @Test
+    void testGetCustomer() {}
+
+    @Test
+    void testRecordSale() {}
+
+    @Test
+    void testGetSaleDetails() {}
+
+    @Test
+    void testCalculateTotal() {}
 }
