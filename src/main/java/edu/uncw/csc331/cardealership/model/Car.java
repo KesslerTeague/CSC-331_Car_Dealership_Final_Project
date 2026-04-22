cf6644191041ffe0dd63ccc6eef2c9290183b36d
arwarden22 <a.r.warden22@gmail.com>
2026-04-22 14:48:54 -0400

    Car Class Structure
    Added fields for number of doors and body style, and updated constructor and methods.

--- src/main/java/edu/uncw/csc331/cardealership/model/Car.java
+++ src/main/java/edu/uncw/csc331/cardealership/model/Car.java
@@ -1,4 +1,24 @@
 package edu.uncw.csc331.cardealership.model;
 
 public class Car extends Vehicle {
+    private int numDoors;
+    private String bodyStyle;
+
+    public void Car(String vehicleId, String make, String model, int year, double price, int numDoors, String bodyStyle) {
+        super(vehicleId, make, model, year, price,);
+        this.numDoors = numDoors;
+        this.bodyStyle = bodyStyle;
+    }
+
+    public String getDetails() {
+        return "Vehicle ID: " + this.vehicleId + "Make: " + this.make + "Model: " + this.model + "Year: " + this.year + "Price: " + this.price + "Number of Doors: " + this.numDoors + "Body Style: " + this.bodyStyle;
+    }
+
+    public void updateStatus() {
+        return;
+    }
+
+    public VehicleStatus getStatus() {
+        return VehicleStatus.getStatus();
+    }
 }
