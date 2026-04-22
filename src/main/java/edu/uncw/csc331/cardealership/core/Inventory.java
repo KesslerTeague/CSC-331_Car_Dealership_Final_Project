c6aa406208e4fac28228757a81f21603d27971cc
Connor <connwarnick@gmail.com>
2026-04-22 08:57:09 -0400

    stub out sale and inventory model classes

--- src/main/java/edu/uncw/csc331/cardealership/core/Inventory.java
+++ src/main/java/edu/uncw/csc331/cardealership/core/Inventory.java
@@ -1,4 +1,29 @@
 package edu.uncw.csc331.cardealership.core;
 
+import edu.uncw.csc331.cardealership.model.Sale;
+import edu.uncw.csc331.cardealership.model.Vehicle;
+import edu.uncw.csc331.cardealership.model.VehicleStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Inventory {
+    private List<Vehicle> vehicles = new ArrayList<>();
+    private List<Sale> sales = new ArrayList<>();
+
+    public void addVehicle(Vehicle v) {}
+
+    public void removeVehicle(Vehicle v) {}
+
+    public List<Vehicle> searchByMake(String make) { return new ArrayList<>(); }
+
+    public List<Vehicle> filterByStatus(VehicleStatus status) { return new ArrayList<>(); }
+
+    public void updateVehicle(Vehicle v) {}
+
+    public List<Vehicle> viewAll() { return new ArrayList<>(vehicles); }
+
+    public void recordSale(Sale sale) {}
+
+    public List<Sale> getSales() { return new ArrayList<>(sales); }
 }
--- src/main/java/edu/uncw/csc331/cardealership/model/Sale.java
+++ src/main/java/edu/uncw/csc331/cardealership/model/Sale.java
@@ -1,4 +1,30 @@
 package edu.uncw.csc331.cardealership.model;
 
+import java.time.LocalDate;
+
 public class Sale {
+    private String saleId;
+    private LocalDate saleDate;
+    private double totalPrice;
+    private Vehicle vehicle;
+    private Customer customer;
+
+    public Sale(String saleId, LocalDate saleDate, Vehicle vehicle, Customer customer) {
+        this.saleId = saleId;
+        this.saleDate = saleDate;
+        this.vehicle = vehicle;
+        this.customer = customer;
+    }
+
+    public String getSaleId() { return saleId; }
+    public LocalDate getSaleDate() { return saleDate; }
+    public double getTotalPrice() { return totalPrice; }
+    public Vehicle getVehicle() { return vehicle; }
+    public Customer getCustomer() { return customer; }
+
+    public void recordSale() {}
+
+    public String getSaleDetails() { return ""; }
+
+    public double calculateTotal() { return 0; }
 }
