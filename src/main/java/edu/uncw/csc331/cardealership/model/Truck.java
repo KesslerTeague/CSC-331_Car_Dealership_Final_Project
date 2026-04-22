bc48d417b6836c96b38766bb5969bae57e035a86
arwarden22 <a.r.warden22@gmail.com>
2026-04-22 14:50:11 -0400

    Truck Class Structure
    Added cabStyle and cargoCapacity attributes with constructor and details method.

--- src/main/java/edu/uncw/csc331/cardealership/model/Truck.java
+++ src/main/java/edu/uncw/csc331/cardealership/model/Truck.java
@@ -1,4 +1,24 @@
 package edu.uncw.csc331.cardealership.model;
 
 public class Truck extends Vehicle {
+  private String cabStyle;
+  private double cargoCapacity;
+  
+  public void Truck(String vehicleId, String make, String model, int year, double price, String cabStyle, double cargoCapacity) {
+      super(vehicleId, make, model, year, price);
+      this.cabStyle = cabStyle;
+      this.cargoCapacity = cargoCapacity;
+    }
+
+  public String getDetails() {
+      return "Vehicle ID: " + this.vehicleId + "Make: " + this.make + "Model: " + this.model + "Year: " + this.year + "Price: " + this.price + "Cab Style: " + this.cabStyle + "Cargo Capacity: " + this.cargoCapacity;
+    }
+
+  public void updateStatus() {
+      return;
+    }
+
+  public VehicleStatus getStatus() {
+      return VehicleStatus.getStatus();
+    }
 }
